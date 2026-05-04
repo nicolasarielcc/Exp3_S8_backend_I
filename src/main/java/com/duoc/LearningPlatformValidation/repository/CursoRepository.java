a47d54f9dc8a2d300e142faab559d590cf7c592a
Nicolas Ariel <ni.cavieres@duocuc.cl>
2026-05-04 01:03:08 -0400

    refactor de CursoRepository por JPA

--- src/main/java/com/duoc/LearningPlatformValidation/repository/CursoRepository.java
+++ src/main/java/com/duoc/LearningPlatformValidation/repository/CursoRepository.java
@@ -1,8 +1,6 @@
 package com.duoc.LearningPlatformValidation.repository;
 import com.duoc.LearningPlatformValidation.model.CursoEntity;
 
-import java.util.ArrayList;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
